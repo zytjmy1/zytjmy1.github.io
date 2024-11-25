4e22526cff5ef67583908d7d23eb781210c753b2
zytjmy1 <sr62200332023@outlook.com>
2024-11-25 23:55:00 +0800

    l

--- server.js
+++ server.js
@@ -34,5 +34,5 @@ app.post('/api/xai/chat', async (req, res) => {
 
 // 启动服务器
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${POST}`);
+  console.log(`Server is running on port ${PORT}`);
 });
